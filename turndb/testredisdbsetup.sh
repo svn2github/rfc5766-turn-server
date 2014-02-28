eed62305ca8b8cf1d9c6f5f9a5b909888eb8c508
mom040267@gmail.com <mom040267@gmail.com@d3c6d0af-6456-5cbd-3218-6188ca4e956b>
2014-02-28 21:32:43 +0000

    redis db
    git-svn-id: http://rfc5766-turn-server.googlecode.com/svn/trunk@1401 d3c6d0af-6456-5cbd-3218-6188ca4e956b

--- turndb/testredisdbsetup.sh
+++ turndb/testredisdbsetup.sh
@@ -2,7 +2,7 @@
 
 redis-cli <<!
 
-select 0 
+SELECT 0
 AUTH turn
 
 set turn/user/ninefingers/key "bc807ee29df3c9ffa736523fb2c4e8ee"
@@ -17,5 +17,25 @@ set turn/denied-peer-ip/123456 "172.17.13.133-172.17.14.56"
 set turn/denied-peer-ip/234567 "123::45"
 
 set turn/allowed-peer-ip/345678 "172.17.13.200"
+
+set turn/origin/http://crinna.org crinna.org
+set turn/origin/http://bligh.edu crinna.org
+
+set turn/realm/crinna.org/db 7
+
+save
+
+SELECT 7
+AUTH turn
+
+set turn/user/whirrun/key "6972e85e51f36e53b0b61759c5a5219a"
+set turn/user/stranger-come-knocking/key "d43cb678560259a1839bff61c19de15e"
+
+set turn/user/whirrun/password "sword"
+set turn/user/stranger-come-knocking/password "civilization"
+
+set turn/secret/777888999 "civilization"
+
+save
 
 !
